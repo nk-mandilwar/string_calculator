880c33d7082bebcb5e921ebee994a665ec94f9de
SneTin <snetin@SneTins-MacBook-Air.local>
2025-10-16 17:44:49 +0530

    Code : update method to raise argument error when argument is not a string

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,7 @@
 class StringCalculator
   def self.add(numbers)
     raise ArgumentError, "argument cannot be nil" if numbers.nil?
+    raise ArgumentError, "argument must be a String" unless numbers.is_a?(String)
     0
   end
 end
