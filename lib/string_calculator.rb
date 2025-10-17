b41a1518183ac837d0b9e2e0f0e255e01633733f
SneTin <snetin@SneTins-MacBook-Air.local>
2025-10-17 16:38:15 +0530

    Code : update method to return he same number when argument string contain single number

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,8 @@ class StringCalculator
   def self.add(numbers)
     raise ArgumentError, "argument cannot be nil" if numbers.nil?
     raise ArgumentError, "argument must be a String" unless numbers.is_a?(String)
-    0
+    return 0 if numbers.empty?
+
+    numbers.to_i
   end
 end
