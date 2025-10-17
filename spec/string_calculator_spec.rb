9f88d0910d8a672a6ec7ee4f3e63290c009cf9ea
SneTin <snetin@SneTins-MacBook-Air.local>
2025-10-17 16:34:48 +0530

    Test : assert to return the same number when argument string contain single number

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,17 +5,21 @@ RSpec.describe StringCalculator do
     it "returns 0 when argument is an empty string" do
       expect(StringCalculator.add("")).to eq(0)
     end
-  end
 
-  it "raises ArgumentError when argument is nil" do
-    expect { StringCalculator.add(nil) }
-      .to raise_error(ArgumentError, "argument cannot be nil")
-  end
+    it "raises ArgumentError when argument is nil" do
+      expect { StringCalculator.add(nil) }
+        .to raise_error(ArgumentError, "argument cannot be nil")
+    end
 
-  it "raises ArgumentError when argument is not a String" do
-    expect { StringCalculator.add(123) }
-      .to raise_error(ArgumentError, "argument must be a String")
-    expect { StringCalculator.add([1, 2, 3]) }
-      .to raise_error(ArgumentError, "argument must be a String")
+    it "raises ArgumentError when argument is not a String" do
+      expect { StringCalculator.add(123) }
+        .to raise_error(ArgumentError, "argument must be a String")
+      expect { StringCalculator.add([1, 2, 3]) }
+        .to raise_error(ArgumentError, "argument must be a String")
+    end
+
+    it "returns the same number when argument string contain single number" do
+      expect(StringCalculator.add("50")).to eq(50)
+    end
   end
 end
