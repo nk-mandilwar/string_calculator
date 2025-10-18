d0420bca26ac942118ecf8dc9167f50bfe84f919
SneTin <snetin@SneTins-MacBook-Air.local>
2025-10-18 10:21:53 +0530

    Test : assert to raise an error for negative numbers and lists them

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -33,5 +33,10 @@ RSpec.describe StringCalculator do
     it "supports custom delimiters defined with //;\n syntax" do
       expect(StringCalculator.add("//;\n1;2")).to eq(3)
     end
+
+    it "raises an error for negative numbers and lists them" do
+      expect { StringCalculator.add("1,-2,3,-5") }
+        .to raise_error("negatives not allowed: -2, -5")
+    end
   end
 end
