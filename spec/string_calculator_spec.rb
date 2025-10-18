16c5549579bc763d61c1b83711f65de1525c3c62
SneTin <snetin@SneTins-MacBook-Air.local>
2025-10-18 10:24:47 +0530

    Test : assert to ignore numbers greater than 1000

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -38,5 +38,9 @@ RSpec.describe StringCalculator do
       expect { StringCalculator.add("1,-2,3,-5") }
         .to raise_error("negatives not allowed: -2, -5")
     end
+
+    it "ignores numbers greater than 1000" do
+      expect(StringCalculator.add("2,1001")).to eq(2)
+    end
   end
 end
