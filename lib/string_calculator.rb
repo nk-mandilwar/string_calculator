e7cc225ce34757911d659d880e67daf8343db4c2
SneTin <snetin@SneTins-MacBook-Air.local>
2025-10-18 11:02:29 +0530

    Code : refactor StringCalculator for clarity and single responsibility

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,25 +1,49 @@
 class StringCalculator
   def self.add(numbers)
-    raise ArgumentError, "argument cannot be nil" if numbers.nil?
-    raise ArgumentError, "argument must be a String" unless numbers.is_a?(String)
+    validate_input(numbers)
     return 0 if numbers.empty?
 
-    delimiter = /,|\n/
-    if numbers.start_with?("//")
-      header, numbers = numbers.split("\n", 2)
-      if header.match(%r{//\[(.+)\]})
-        delimiters = header.scan(%r{\[(.*?)\]}).flatten.map { |d| Regexp.escape(d) }
-        delimiter = Regexp.new(delimiters.join("|"))
-      else
-        custom = header[2..]
-        delimiter = Regexp.new(Regexp.escape(custom))
-      end
-    end
+    delimiter, numbers = extract_delimiter(numbers)
+    nums = parse_numbers(numbers, delimiter)
 
-    nums = numbers.split(delimiter).map(&:to_i)
-    negatives = nums.select { |n| n < 0 }
-    raise "negatives not allowed: #{negatives.join(', ')}" if negatives.any?
+    check_for_negatives(nums)
 
     nums.reject { |n| n > 1000 }.sum
+  end
+
+  private
+
+  def self.validate_input(numbers)
+    raise ArgumentError, "argument cannot be nil" if numbers.nil?
+    raise ArgumentError, "argument must be a String" unless numbers.is_a?(String)
+  end
+
+  def self.extract_delimiter(numbers)
+    default_delimiter = /,|\n/
+
+    return [default_delimiter, numbers] unless numbers.start_with?("//")
+
+    header, numbers = numbers.split("\n", 2)
+    delimiter = parse_header_delimiter(header)
+    [delimiter, numbers]
+  end
+
+  def self.parse_header_delimiter(header)
+    if header.match(%r{//\[(.+)\]})
+      delimiters = header.scan(%r{\[(.*?)\]}).flatten.map { |d| Regexp.escape(d) }
+      Regexp.new(delimiters.join("|"))
+    else
+      custom = header[2..]
+      Regexp.new(Regexp.escape(custom))
+    end
+  end
+
+  def self.parse_numbers(numbers, delimiter)
+    numbers.split(delimiter).map(&:to_i)
+  end
+
+  def self.check_for_negatives(nums)
+    negatives = nums.select { |n| n < 0 }
+    raise "negatives not allowed: #{negatives.join(', ')}" if negatives.any?
   end
 end
