829825f22623b0d25a8d0a9be0e203d2b341fe5e
SneTin <snetin@SneTins-MacBook-Air.local>
2025-10-18 10:46:41 +0530

    Test : assert to support multiple delimiters

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -46,5 +46,9 @@ RSpec.describe StringCalculator do
     it "supports delimiters of any length" do
       expect(StringCalculator.add("//[***]\n1***2***3")).to eq(6)
     end
+
+    it "supports multiple delimiters" do
+      expect(StringCalculator.add("//[*][%]\n1*2%3")).to eq(6)
+    end
   end
 end
