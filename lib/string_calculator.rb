d3738b93aa4396b2c1cda1d15399c20ea8fb2657
nk-mandilwar <nitinkumarmandilwar5@gmail.com>
2025-11-01 05:54:34 +0530

    Code : update method to handle multiplication when * delimeter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,54 +1,78 @@
 class StringCalculator
-  def self.add(numbers)
-    validate_input(numbers)
-    return 0 if numbers.empty?
+  class << self
+    def add(numbers)
+      validate_input(numbers)
+      return 0 if numbers.strip.empty?
 
-    delimiter, numbers = extract_delimiter(numbers)
+      delimiters, numbers = extract_delimiters(numbers)
+      validate_delimiters!(delimiters)
 
-    tokens = numbers.split(delimiter).map(&:strip)
-    validate_tokens!(tokens)
+      tokens = tokenize(numbers, delimiters)
+      validate_tokens!(tokens)
 
-    nums = tokens.map(&:to_i)
-    check_for_negatives(nums)
-    nums.reject { |n| n > 1000 }.sum
-  end
+      values = tokens.map(&:to_i)
+      check_negatives!(values)
 
-  private
+      values.reject! { |n| n > 1000 }
 
-  def self.validate_input(numbers)
-    raise ArgumentError, "argument cannot be nil" if numbers.nil?
-    raise ArgumentError, "argument must be a String" unless numbers.is_a?(String)
-  end
-
-  def self.extract_delimiter(numbers)
-    default_delimiter = /,|\n/
-    return [default_delimiter, numbers] unless numbers.start_with?("//")
-
-    header, numbers = numbers.split("\n", 2)
-    [parse_header_delimiter(header), numbers]
-  end
-
-  def self.parse_header_delimiter(header)
-    if header.include?("-")
-      raise ArgumentError, "invalid delimiter: '-' cannot be used as it conflicts with negative numbers"
+      # Multiply only if *-delimiters exclusively used, else sum
+      if delimiters.all? { |d| d.include?('*') }
+        multiply_then_sum(numbers, delimiters)
+      else
+        values.sum
+      end
     end
 
-    if header.match(%r{//\[(.+)\]})
-      delimiters = header.scan(%r{\[(.*?)\]}).flatten.map { |d| Regexp.escape(d) }
-      Regexp.new(delimiters.join("|"))
-    else
-      Regexp.new(Regexp.escape(header[2]))
-    end
-  end
+    private
 
-  def self.validate_tokens!(tokens)
-    unless tokens.all? { |t| t.match?(/\A-?\d+\z/) }
-      raise ArgumentError, "input contains invalid characters or partial delimiters"
+    def validate_input(numbers)
+      raise ArgumentError, "argument cannot be nil" if numbers.nil?
+      raise ArgumentError, "argument must be a String" unless numbers.is_a?(String)
     end
-  end
 
-  def self.check_for_negatives(nums)
-    negatives = nums.select(&:negative?)
-    raise "negatives not allowed: #{negatives.join(', ')}" if negatives.any?
+    def validate_delimiters!(delimiters)
+      if delimiters.include?('-')
+        raise ArgumentError, "invalid delimiter: '-' cannot be used as it conflicts with negative numbers"
+      end
+    end
+
+    def validate_tokens!(tokens)
+      tokens.each do |token|
+        next if token.strip.empty?
+        unless token.strip.match?(/\A-?\d+\z/)
+          raise ArgumentError, "input contains invalid characters or partial delimiters"
+        end
+      end
+    end
+
+    def check_negatives!(values)
+      negatives = values.select(&:negative?)
+      raise "negatives not allowed: #{negatives.join(', ')}" if negatives.any?
+    end
+
+    def extract_delimiters(input)
+      return [[",", "\n"], input] unless input.start_with?("//")
+
+      header, body = input.split("\n", 2)
+      delimiters = header.scan(/\[(.*?)\]/).flatten
+      delimiters = [header[2]] if delimiters.empty?
+      [delimiters, body]
+    end
+
+    def tokenize(numbers, delimiters)
+      numbers.strip.split(Regexp.union(delimiters))
+    end
+
+    def multiply_then_sum(input, delimiters)
+      mult_delims = delimiters.select { |d| d.include?('*') }
+      add_delims  = delimiters - mult_delims
+
+      add_parts = add_delims.empty? ? [input] : input.split(Regexp.union(add_delims))
+
+      add_parts.sum do |part|
+        nums = part.split(Regexp.union(mult_delims)).map(&:to_i).reject { |n| n > 1000 }
+        nums.empty? ? 0 : nums.inject(:*)
+      end
+    end
   end
 end
